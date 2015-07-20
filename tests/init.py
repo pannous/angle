2eebacc20d17704fd39c482d0c3d7ad2a5ad7312
Pannous <info@pannous.com>
2015-07-20 10:00:00 +0200

    ''

--- tests/init.py
+++ tests/init.py
@@ -1,14 +1,14 @@
-from discover import DiscoveringTestLoader
-
-
-def get_tests():
-    start_dir = os.path.dirname(__file__)
-    test_loader = DiscoveringTestLoader()
-    return test_loader.discover(start_dir, pattern="*_test.py")
-
-# import os.path
-# import unittest
+# from discover import DiscoveringTestLoader
+# 
 # 
 # def get_tests():
 #     start_dir = os.path.dirname(__file__)
-#     return unittest.TestLoader().discover(start_dir, pattern="*_test.py")
+#     test_loader = DiscoveringTestLoader()
+#     return test_loader.discover(start_dir, pattern="*_test.py")
+
+import os.path
+import unittest
+
+def get_tests():
+    start_dir = os.path.dirname(__file__)
+    return unittest.TestLoader().discover(start_dir, pattern="*_test.py")
