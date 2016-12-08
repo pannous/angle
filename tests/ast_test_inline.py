4abee248b4238d5ac7f74142c255046ade3d72f4
Pannous <info@pannous.com>
2016-12-09 00:03:28 +0100

    better ast

--- tests/ast_test_inline.py
+++ tests/ast_test_inline.py
@@ -1,40 +1,34 @@
 #!/usr/local/bin/python
-
-
-
 import ast
 import os
-# from ast import *
 import ipython_genutils.py3compat
 import sys
+from astor import codegen
 
 if 'TESTING' in os.environ:
 	quit(1)
-	# tests.parser_test_helper.skip()
-	# raise Warning("Don't Test this")
 
-# from kast import *
-# import astor
-# from angle import english_parser
-# from angle.nodes import Argument
-from astor import codegen
-
-do_exec =False
-
+ast_file = "out/inline.ast"
+do_exec = False
+arg = None
 if len(sys.argv) > 1:
-	expr = arg = sys.argv[1]
-	if expr.endswith(".py"):
-		expr=open(expr).read()
-		do_exec=True
-	my_ast = ast.parse(expr)
-	print(expr)
+	arg = sys.argv[1]
+
+if arg and ".ast" in arg:
+	ast_file = arg
+elif arg:
+	if arg.endswith(".py"):
+		expr = open(arg).read()
+		do_exec = True
+	my_ast = ast.parse(arg)
+	print(arg)
 	print("")
 	x = ast.dump(my_ast, annotate_fields=True, include_attributes=True)
 	print(x)
-	print("\n--------MEDIUM--------\n")
+	print("\n--------MEDIUM-------- (ok)\n")
 	x = ast.dump(my_ast, annotate_fields=True, include_attributes=False)
 	print(x)
-	print("\n--------SHORT--------\n")
+	print("\n--------SHORT-------- (view-only)\n")
 	x = ast.dump(my_ast, annotate_fields=False, include_attributes=False)
 	print(x)
 	print("")
@@ -43,13 +37,15 @@ if len(sys.argv) > 1:
 
 try:
 	inline_ast = "set in execfile:"
-	ipython_genutils.py3compat.execfile("out/inline.ast", glob={})
-	source = codegen.to_source(inline_ast)
-	print(source)  # => CODE
+	exec(open(ast_file).read())
+	try:
+		source = codegen.to_source(inline_ast)
+		print(source)  # => CODE
+	except Exception as ex:
+		print(ex)
 	my_ast = ast.fix_missing_locations(inline_ast)
 	code = compile(inline_ast, "out/inline", 'exec')
-except Exception as e:
-	print(e)
-# raise
-if do_exec:
+	# if do_exec:
 	exec(code)
+except Exception as e:
+	raise
