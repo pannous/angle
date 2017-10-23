04ed538c9ff6869a39e04afc9f7272d54513a3c2
Pannous <info@pannous.net>
2017-10-24 01:00:24 +0200

    test.js

---
+++ angle.js/test/power_parser.test.js
@@ -0,0 +1,17 @@
+"use strict";
+require('../english_parser')
+exports.testSomething = function(test) {
+    test.expect(1);
+    test.ok(true, "this assertion should pass");
+    test.done();
+};
+
+// const sum = require('./sum');
+const sum = (a, b) => a + b;
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('parser', () => {
+     parse('1+1=2')
+});
